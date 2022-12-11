c6fe6a47b2e3a25c4d29f20395fbfb0683848b8c
Are Schjetne <sixcare.as@gmail.com>
2022-12-11 19:43:12 +0100

    Working part 1

--- 04/camp-clean.f90
+++ 04/camp-clean.f90
@@ -2,13 +2,10 @@ module utils
     implicit none
 contains
 function array_from_file(name, mode)
-
     character(len=*), intent(in) :: name
     character(len=*), intent(in) :: mode
-    character(len=7), allocatable :: arr(:), tmp(:)
-    character(len=4), allocatable :: array_from_file(:)
-
-    character(len=7) :: line
+    character(len=11), allocatable :: arr(:), tmp(:), array_from_file(:)
+    character(len=11) :: line
     integer :: i, ios, array_len
 
     array_len = 9000
@@ -42,7 +39,7 @@ function array_from_file(name, mode)
             deallocate(tmp)
         endif
 
-        arr(i) = line(1:1)//line(3:3)//line(5:5)//line(7:7)
+        arr(i) = line
 
         i = i + 1
     end do i_loop
@@ -53,44 +50,74 @@ function array_from_file(name, mode)
 
     return
 end function array_from_file
+
+function sectors_from_array(arr)
+    character(len=11), dimension(:), intent(in)  :: arr ! input
+    character(len=11) :: line
+    integer, dimension(:, :), allocatable :: sectors_from_array, sector_tmp
+    integer :: i, array_len, sector_start, sector_end, pos_dash, pos_com
+
+    array_len = size(arr) -1
+
+    allocate(sector_tmp(array_len,4))
+    
+    do i = 1, array_len
+        line = arr(i)
+        pos_com = scan(line, ',')
+
+        pos_dash = scan(line, '-')
+        read(line(1:pos_dash-1),*) sector_start
+        read(line(pos_dash+1:pos_com-1),*) sector_end
+        sector_tmp(i,1) = sector_start
+        sector_tmp(i,2) = sector_end
+
+        pos_dash = scan(line, '-', back=.true.)
+        read(line(pos_com+1:pos_dash-1),*) sector_start
+        read(line(pos_dash+1:len_trim(line)),*) sector_end
+        sector_tmp(i,3) = sector_start
+        sector_tmp(i,4) = sector_end
+    end do
+
+    allocate(sectors_from_array(i-1, 4))
+    sectors_from_array(1:i-1,1:4) = sector_tmp(1:i-1,1:4)
+    deallocate(sector_tmp)
+end function sectors_from_array
 end module utils
 
 
 program camp_clean
     use utils
     implicit none
-    character(len=4), allocatable :: arr(:)
-    integer :: i, j, result, len
+    character(len=11), allocatable :: arr(:)
+    integer, dimension(:, :), allocatable :: sectors
+    integer :: i, j, k, result, len
     integer :: sec11, sec12, sec21, sec22, longest_start, longest_end, shortes_start, shortes_end
     logical :: start, end
 
-    arr = array_from_file('input-test', '(A)')
+    arr = array_from_file('input', '(A)')
     len = size(arr) - 1
+
+    sectors = sectors_from_array(arr)
+
     result = 0
 
-    i_loop: do i = 1, len
-        read(arr(i)(1:1),*) sec11
-        read(arr(i)(2:2),*) sec12
-        read(arr(i)(3:3),*) sec21
-        read(arr(i)(4:4),*) sec22
-        !print *, arr(i), i, sec11, arr(i)(1:1), sec12, arr(i)(2:2), sec21, arr(i)(3:4), sec22, arr(i)(4:4)
-
+    i_loop: do i = 1, size(sectors)/4
         longest_start = 0
         longest_end = 0
         shortes_start = 0
         shortes_end = 0
         start = .false.
         end = .false.
-        if (sec12-sec11 > sec22-sec21) then
-            longest_start = sec11
-            longest_end = sec12
-            shortes_start = sec21
-            shortes_end = sec22
+        if (sectors(i, 2)-sectors(i, 1) > sectors(i, 4)-sectors(i, 3)) then
+            longest_start = sectors(i, 1)
+            longest_end = sectors(i, 2)
+            shortes_start = sectors(i, 3)
+            shortes_end = sectors(i, 4)
         else
-            longest_start = sec21
-            longest_end = sec22
-            shortes_start = sec11
-            shortes_end = sec12
+            longest_start = sectors(i, 3)
+            longest_end = sectors(i, 4)
+            shortes_start = sectors(i, 1)
+            shortes_end = sectors(i, 2)
         endif
         j_loop: do j = longest_start, longest_end
             if (j == shortes_start) then
